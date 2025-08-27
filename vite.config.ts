0845136a40d14391655aba4d483467ddcc1dd144
AzamBaltistani <sikanderazam276@gmail.com>
2025-08-27 13:02:32 +0500

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "ExpenseTrackerWebApp_2",
+  base: "/ExpenseTrackerWebApp_2/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
